528fa2b334b749b148e28d27a531d13a0c9c0c70
MohdAmmar10 <mohd.ammarmakki786@gmail.com>
2021-11-08 14:43:40 +0530

    updated test

--- src/Test/example/NewTest.java
+++ src/Test/example/NewTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;		
 import org.testng.annotations.Test;	
 import org.testng.annotations.BeforeTest;	
-import org.testng.annotations.AfterTest;		
+import org.testng.annotations.AfterTest;
 public class NewTest {		
 	    private WebDriver driver;		
 		@Test				
